d9d99f2ff0e1d289254c0081e231783d7c2d4ed6
Juanesfa <juanesfa210907@gmail.com>
2024-12-02 20:00:08 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'', //🏠Página de Inicio
+    loadComponent:() => import ('./Views/home/home.component').then(m =>   m.HomeComponent)
+  },
+
+  {
+    path:'about', //ℹ️ Acerca de 
+    loadComponent:() => import ('./Views/about/about.component').then(m => m.AboutComponent)
+  },
+
+  {
+    path:'contact', //📬 Contacto
+    loadComponent:() => import ('./Views/contact/contact.component').then(m => m.ContactComponent)
+  },
+
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
